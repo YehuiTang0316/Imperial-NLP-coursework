ba375638a5fd78103a00257a728abc0972b39e86
pinkyhsu <ipinkyhsu@gmail.com>
2021-02-20 23:06:27 +0800

    get_edited

---
+++ get_edited_headline.py
@@ -0,0 +1,7 @@
+import re
+
+def get_edited_df(df):
+    df_copy = df.copy(deep=True)
+    df_copy['edited']=df_copy['original']
+    df_copy['edited'] = df_copy.apply(lambda x: re.sub(r"(<[^>]+>)", x['edit'], x['edited']), axis=1)
+    return df_copy
